1aa8117e033c13210dba52a07e7d78b41bd0eea6
dasdeba2607 <dasdeba@hotmail.com>
2023-01-31 06:53:26 +0000

    Update student.py
    Created json file using file IO

--- student.py
+++ student.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 def GetInputFromStudent():
     global lname, lage, lemail, lcourseid
     lname = input("please enter name, then enter")
@@ -26,6 +29,7 @@ def StoreStudentDataIntoDict():
     student_dict = {}
     studentMaster1 = StudentMaster(lname,lage,lemail,lcourseid)
     student_dict[lname]=studentMaster1
+    #print(student_dict[lname].name)
 
 
 def PrintData():
@@ -35,9 +39,38 @@ def PrintData():
         print("email is ", student_list[0]['email'])
         print("course is ", student_list[0]['course'])
 
+def StoreDataToFile():
+    global final_dict
+    final_dict= {}
+    os.chdir("C:/deba/deba5/training/python/pythonTrainingProject")
+    mycurdir = os.getcwd()
+    if mycurdir != "C:\deba\deba5\\training\python\pythonTrainingProject":
+        print("not a correct directory, doing nothing")
+    else:
+        print("correct directory, doing nothing")
+    try:
+        print(student_dict.keys())
+        fo = open("das.txt", "w+")
+        # print(type(fo))
+        # print(fo.mode)
+        for r in student_dict.keys():
+            #final_dict[r]={"age":str(student_dict[r].name,"email":str(student_dict[r].email,"course":str(student_dict[r].course}
+            myvar = "{ \"" + r + "\" : { \"age\" : \"" + student_dict[r].age + "\", \"email\" : \"" + student_dict[r].email + "\", \"course\" : \"" + student_dict[r].course + "\" }}"
+            print(myvar)
+            fo.write(myvar)
+        fo.close()
+    except FileNotFoundError as e:
+        print("unable to open file ", sys.exc_info()[0], sys.exc_info()[1])
+    except PermissionError as e:
+        print("permission error", sys.exc_info()[0], sys.exc_info()[1])
+    except IsADirectoryError:
+        print("Error related to directory", sys.exc_info()[0], sys.exc_info()[1])
+
+
 
 if __name__ == '__main__':
     GetInputFromStudent()
     #StoreStudentData()
     StoreStudentDataIntoDict()
-    PrintData()
+    #PrintData()
+    StoreDataToFile()
