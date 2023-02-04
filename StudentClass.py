0544cc1bd52703408225ea27b7f96ebdffcb3244
dasdeba2607 <dasdeba@hotmail.com>
2023-02-04 16:21:39 +0000

    Create StudentClass.py

---
+++ StudentClass.py
@@ -0,0 +1,7 @@
+class StudentMaster:
+    def __init__(self, lname, lage, lemail, lcourse, lstatus):
+        self.name = lname
+        self.age = lage
+        self.email = lemail
+        self.course = lcourse
+        self.status = lstatus
