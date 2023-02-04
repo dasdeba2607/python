03653f3bf54778c61c169beb57830684c3fc19d9
dasdeba2607 <dasdeba@hotmail.com>
2023-02-04 16:21:25 +0000

    Create myjon.py

---
+++ myjon.py
@@ -0,0 +1,8 @@
+with open('das.txt') as f:
+    lines = [line for line in f]
+    # print(lines)
+    # mylist=[]
+    # f.seek(0,0)
+    # for r in f:
+    #     mylist.append(r)
+    # print(mylist)
