fe803a2f840fc89762292bf425a69c0223351aa2
dasdeba2607 <123736334+dasdeba2607@users.noreply.github.com>
2023-02-10 12:41:42 +0000

    Update database.py
    file file with static method only

--- database.py
+++ database.py
@@ -1,27 +1,65 @@
+import os
 
-def StoreDataToFile():
-    global final_dict
-    final_dict= {}
-    os.chdir("C:/deba/deba5/training/python/pythonTrainingProject")
-    mycurdir = os.getcwd()
-    if mycurdir != "C:\deba\deba5\\training\python\pythonTrainingProject":
-        print("not a correct directory, doing nothing")
-    else:
-        print("correct directory, doing nothing")
-    try:
-        print(student_dict.keys())
-        fo = open("das.txt", "w+")
-        # print(type(fo))
-        # print(fo.mode)
-        for r in student_dict.keys():
-            #final_dict[r]={"age":str(student_dict[r].name,"email":str(student_dict[r].email,"course":str(student_dict[r].course}
-            myvar = "{ \"" + r + "\" : { \"age\" : \"" + student_dict[r].age + "\", \"email\" : \"" + student_dict[r].email + "\", \"course\" : \"" + student_dict[r].course + "\" }}"
-            print(myvar)
-            fo.write(myvar)
-        fo.close()
-    except FileNotFoundError as e:
-        print("unable to open file ", sys.exc_info()[0], sys.exc_info()[1])
-    except PermissionError as e:
-        print("permission error", sys.exc_info()[0], sys.exc_info()[1])
-    except IsADirectoryError:
-        print("Error related to directory", sys.exc_info()[0], sys.exc_info()[1])
+from StudentClass import StudentMaster
+
+DELIMETER = ","
+fileName = "db.txt"
+
+if os.path.isfile(fileName):
+    file = open(fileName, "r+")
+else:
+    file = open(fileName, "w+")
+
+
+class Database:
+
+    @staticmethod
+    def insert():
+        lname = input("please enter name, then enter")
+        lage = input("please enter age, then enter")
+        lemail = input("please enter email, then enter")
+        lcourseid = input("please enter course id, then enter")
+        lstatus = input("please enter Admission status [A/D] :, then enter")
+        studentdata = StudentMaster(lname, lage, lemail, lcourseid, lstatus)
+        record = studentdata.name + DELIMETER + studentdata.age + DELIMETER + studentdata.email + DELIMETER + studentdata.course + DELIMETER + studentdata.status
+        i = -1
+        for i, line in enumerate(file):
+            pass
+        print("line no: ", i)
+        if file.mode == "r+":
+            if i == -1:
+                file.seek(0, 0)
+            else:
+                file.seek(0, 2)
+                file.write("\n")
+            file.write(record)
+        elif file.mode == "w+":
+            file.writelines(record)
+            file.write("\n")
+        file.seek(0, 0)
+
+    @staticmethod
+    def select():
+        file.seek(0, 0)
+        count = 0
+        for line in file:
+            count += 1
+            print("Record{}: {}".format(count, line.strip()))
+
+    @staticmethod
+    def close():
+        if file.mode == "r+":
+            file.close()
+        elif file.mode == "w+":
+            file.close()
+
+    @staticmethod
+    def search(attr, criteria):
+        file.seek(0, 0)
+        lines = file.readlines()
+        if attr == "name":
+            count = 0
+            for row in lines:
+                count += 1
+                if repr(row).startswith(criteria, 1):
+                    print("Record no {} : {}".format(count, row))
