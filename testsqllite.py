5055d319fbc6edf4425ae100164728042c1ac8be
dasdeba2607 <123736334+dasdeba2607@users.noreply.github.com>
2023-02-11 13:38:31 +0000

    Update testsqllite.py
    using cursor

--- testsqllite.py
+++ testsqllite.py
@@ -3,11 +3,12 @@ import traceback, sys
 
 # the following line creates a db file of sqllite and then connects
 conn = sqlite3.connect('test.db')
+cur =  conn.cursor()
 
 print("test db creation successful")
 
 try:
-   conn.execute('''CREATE TABLE COMPANY
+   cur.execute('''CREATE TABLE COMPANY
          (ID INT PRIMARY KEY     NOT NULL,
          NAME           TEXT    NOT NULL,
          AGE            INT     NOT NULL,
@@ -23,7 +24,7 @@ except sqlite3.Error as er:
         print("table did not create successfully")
 
 
-conn.execute("DELETE from COMPANY where ID = 1")
+cur.execute("DELETE from COMPANY")
 
 conn.commit()
 
@@ -40,32 +41,30 @@ while ctr < 3:
       address=input("address")
       salary=input("salary")
       sql_data = (id, name, age, address, salary)
-      conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)  VALUES (?, ?, ?, ?, ? )", (sql_data));
+      cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)  VALUES (?, ?, ?, ?, ? )", (sql_data))
       conn.commit()
       print("record inserted successfully :", ctr)
       ctr+=1
    except sqlite3.IntegrityError as e:
       print('INTEGRITY ERROR\n')
 
-cursor = conn.execute("select count(*) from Company")
+result = cur.execute("select count(*) from Company")
 
-for r in cursor:
+for r in result:
     print("the no of records in table ", r[0])
 
-conn.execute("UPDATE COMPANY set SALARY = 25000.00 where ID = 1")
+cur.execute("UPDATE COMPANY set SALARY = 25000.00 where ID = 1")
 
 conn.commit()
 
 print("Total number of rows updated :", conn.total_changes)
 
-cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+result = cur.execute("SELECT id, name, address, salary from COMPANY")
 
-for row in cursor:
+for row in result:
    print("ID = ", row[0])
    print("NAME = ", row[1])
    print("ADDRESS = ", row[2])
    print("SALARY = ", row[3])
-
-cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
 
 conn.close()
