4829998240fccfa9e519aa0c1e251407ba516bec
dasdeba2607 <123736334+dasdeba2607@users.noreply.github.com>
2023-03-03 09:22:27 +0000

    Create formsexample.py

---
+++ WTForms/formsexample.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+
+app = Flask(__name__)
+app.secret_key = 'development key'
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        print(form.validate())
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('contact1.html', form=form)
+        else:
+            return render_template('success.html')
+    elif request.method == 'GET':
+        return render_template('contact1.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
